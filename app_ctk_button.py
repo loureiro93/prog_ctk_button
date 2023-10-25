f7e758643f9fce309c1cd24df6a71e1e5a400734
loureiro93 <126248394+loureiro93@users.noreply.github.com>
2023-10-25 15:22:34 +0100

    Versão tkinter.com

--- app_ctk_button.py
+++ app_ctk_button.py
@@ -1,12 +1,33 @@
 import customtkinter
 
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
 def button_callback():
-    print("button clicked")
+    label.configure(text=button.cget('text'))
 
-app = customtkinter.CTk()
-app.geometry("400x150")
+root = customtkinter.CTk()
+root.geometry("600x350")
+#root.iconbitmap('Imagens/maedoPer.jpeg')
+root.title('Custom TKinter Buttons')
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.pack(padx=20, pady=20)
+button = customtkinter.CTkButton(
+    root, text="Hello World!!!!", 
+    command=button_callback,
+    height=100,
+    width=200,
+    font=('Helvetica', 24),
+    text_color='black',
+    fg_color='red',
+    hover_color='green',
+    corner_radius=50,
+    bg_color='white',
+    border_width=10,
+    border_color='yellow',
+    state='normal')
+button.pack(pady=80)
 
-app.mainloop()
+label = customtkinter.CTkLabel(root, text='')
+label.pack(pady=20)
+
+root.mainloop()
